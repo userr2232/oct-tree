8d6bd5aa180cae2648b23fcda68eeb0a0552e44a
indhira15 <indhira.ramirez@utec.edu.pe>
2020-12-12 17:35:36 -0500

    added

--- main.cc
+++ main.cc
@@ -1,5 +1,5 @@
 #include "node.h"
-#include "Cimg.h"
+#include "CImg.h"
 #include <vector>
 #include <iostream>
 #include <string>
@@ -7,26 +7,31 @@
 #include <utility>
 
 using namespace std;
+using namespace cimg_library;
+//#define cimg_use_jpeg 1
+// g++ -std=c++17  main.cc -I /opt/X11/include -L /opt/X11/lib -lX11 -ljpeg -lpng -lz -pthread
 
 class Octree {
     private:
-        fstream save_file;
+        string oct_file;
         long EPS = 2;
         Node root;
 
-        vector<const char*> get_file_names(const char* fnames) {
+        vector<string> get_file_names(const char* fnames) {
             string fname;
-            vector<const char*> res;
-            while(getline(cin, fname))
-                res.push_back(fname.c_str());
+            vector<string> res;
+            ifstream file(fnames);
+            while(getline(file, fname)){
+                res.push_back(fname);
+            }
             return res;
         }
 
         void build(const char* fnames) {
-            vector<const char*> v = get_file_names(fnames);
-            vector<CImg<unsigned char>> images;
+            vector<string> v = get_file_names(fnames);
+            vector<CImg<float>> images;
             for(auto x : v)
-                images.push_back(CImg<unsigned char>(x));
+                images.push_back(CImg<float>(x.c_str()));
             allocate_node(0, 0, 0, images[0].width(), images[0].height(), images.size(), images);
             read_root();
             insert(root, images);
@@ -34,13 +39,15 @@ class Octree {
 
         Node read(long dir)
         {
+            ifstream save_file(oct_file);
             save_file.seekg(dir);
             Node node{};
             save_file.read((char*)(&node),sizeof(Node));
+            save_file.close();
             return node;
         }
 
-        void insert(Node cur, vector<CImg<unsigned char>>& cube) {
+        void insert(Node cur, vector<CImg<float>>& cube) {
             if(!verificar(cur.x1, cur.y1, cur.z1, cur.x2, cur.y2, cur.z2, cube).first) {
                 divide(cur,cube);
                 for(long i = 0; i < 8; ++i) {
@@ -50,25 +57,28 @@ class Octree {
             }
         }
 
-        long allocate_node(long x1, long y1, long z1, long x2, long y2, long z2,vector<CImg<unsigned char>>& cube)
+        long allocate_node(long x1, long y1, long z1, long x2, long y2, long z2,vector<CImg<float>>& cube)
         {
-            save_file.seekg(0,ios::end);
+            ofstream save_file(oct_file, ios::app | ios::binary);
             Node a(x1,y1,z1,x2,y2,z2,-1);
             a.c=verificar(x1,y1,z1,x2,y2,z2,cube).second;
 
-            long pos=save_file.tellg();
+            long pos=save_file.tellp();
             a.dir=pos;
             save_file.write((char*)(&a),sizeof(Node));
+            save_file.close();
             return pos;
         }
 
         void write(Node cur)
         {
-            save_file.seekg(cur.dir);
+            ofstream save_file(oct_file);
+            save_file.seekp(cur.dir);
             save_file.write((char*)(&cur),sizeof(Node));
-        }
+            save_file.close();
+        } 
 
-        void divide(Node& cur,vector<CImg<unsigned char>>& cube) {
+        void divide(Node& cur,vector<CImg<float>>& cube) {
             int x1=cur.x1;
             int x2=cur.x2;
             int y1=cur.y1;
@@ -76,35 +86,35 @@ class Octree {
             int z1=cur.z1;
             int z2=cur.z2;
 
-            long xm=(cur.x1+cur->x2)/2;
-            long ym=(cur.y1+cur->y2)/2;
-            long zm=(cur->z1+cur->z2)/2;
+            long xm=(cur.x1+cur.x2)/2;
+            long ym=(cur.y1+cur.y2)/2;
+            long zm=(cur.z1+cur.z2)/2;
 
-            cur->children[0] = allocate_node(x1,y1,z1,xm,ym,zm);// AR1(AMARILLO)=X1,Y1,Z1 -> XM,YN,ZM
-            cur->children[1] = allocate_node(x1,ym,z1,xm,y2,zm);//AR2(MORADO)=X1,YM,Z1 -> XM,Y2,ZM
-            cur->children[2] = allocate_node(xm,ym,z1,x2,y2,zm);//AR3(VERDE)=XM,YM,Z1 -> X2,Y2,ZM
-            cur->children[3] = allocate_node(xm,y1,z1,x2,ym,zm);//ARR4(CELESTE)=XM,Y1,Z1 -> X2,YM,ZM
+            cur.children[0] = allocate_node(x1,y1,z1,xm,ym,zm, cube);// AR1(AMARILLO)=X1,Y1,Z1 -> XM,YN,ZM
+            cur.children[1] = allocate_node(x1,ym,z1,xm,y2,zm, cube);//AR2(MORADO)=X1,YM,Z1 -> XM,Y2,ZM
+            cur.children[2] = allocate_node(xm,ym,z1,x2,y2,zm, cube);//AR3(VERDE)=XM,YM,Z1 -> X2,Y2,ZM
+            cur.children[3] = allocate_node(xm,y1,z1,x2,ym,zm, cube);//ARR4(CELESTE)=XM,Y1,Z1 -> X2,YM,ZM
 
-            cur->children[4] = allocate_node(x1,y1,zm,xm,ym,z2);//AB1(AMARILLO)= X1, Y1,ZM -> XM,YM,Z2
-            cur->children[5] = allocate_node(x1,ym,zm,xm,y2,z2);//AB2(BLANCO)= X1,YM,ZM -> XM,Y2,Z2
-            cur->children[6] = allocate_node(xm,y1,zm,x2,ym,z2);//AB3(CELESTE)=XM,Y1,ZM -> X2,YM,Z2
-            cur->children[7] = allocate_node(xm,ym,zm,x2,y2,z2);//AB4(VERDE)=XM,YM,ZM -> X2,Y2,Z2
+            cur.children[4] = allocate_node(x1,y1,zm,xm,ym,z2, cube);//AB1(AMARILLO)= X1, Y1,ZM -> XM,YM,Z2
+            cur.children[5] = allocate_node(x1,ym,zm,xm,y2,z2, cube);//AB2(BLANCO)= X1,YM,ZM -> XM,Y2,Z2
+            cur.children[6] = allocate_node(xm,y1,zm,x2,ym,z2, cube);//AB3(CELESTE)=XM,Y1,ZM -> X2,YM,Z2
+            cur.children[7] = allocate_node(xm,ym,zm,x2,y2,z2, cube);//AB4(VERDE)=XM,YM,ZM -> X2,Y2,Z2
         }
 
-        pair<bool,long> verificar(long x1, long y1, long z1, long x2, long y2, long z2, vector<CImg<unsigned char>>& cube) {
+        pair<bool,long> verificar(long x1, long y1, long z1, long x2, long y2, long z2, vector<CImg<float>>& cube) {
             long color=0;
             if(x2 - x1 <= 1 && y2 - y1 <= 1 && z2 - z1 <= 1)
                 return {true,cube[z1](x1,y1)};
-            unsigned char c = cube[z1](x1, x2);
+            float c = cube[z1](x1, x2);
             for(long i = x1; i < x2; ++i) {
                 for(long j = y1; j < y2; ++j) {
                     for(long k = z1; k < z2; ++k) {
-                        color+=cube[k](i,j);        
+                        //color+=cube[k](i,j);        
                         if(abs(c - cube[k](i,j)) > EPS) return {false,-1};
                     }
                 }
             }
-            return {true,color/((x2-x1)*(y2-y1)*(z2-z1));
+            return {true,c/*/((x2-x1)*(y2-y1)*(z2-z1))*/};
         }
 
         void read_root() {
@@ -113,13 +123,16 @@ class Octree {
 
     public:
         Octree(const char* nodesfile) {
-            save_file.open(nodesfile, fstream::in | fstream::out);
             read_root();
-            save_file.close();
         }
-        Octree(const char* nodesfile, const char* fnames) {
-            save_file.open(nodesfile, fstream::in | fstream::out);
+        Octree(string nodesfile, const char* fnames) {
+            oct_file = nodesfile;
             build(fnames);
-            save_file.close();
         }
 };
+
+int main(){
+    Octree oc("octree","files.txt");
+    Octree oc2("octree");
+    return 0;
+}
--- node.h
+++ node.h
@@ -1,10 +1,11 @@
 struct Node {
-  int x1, y1, z1, x2, y2, z2, c;
+  int x1, y1, z1, x2, y2, z2;
   long dir;
+  float c;
 
   long children[8];
 
   Node() = default;
-  Node(int x1, int y1, int z1, int x2, int y2, int z2, int c): 
+  Node(int x1, int y1, int z1, int x2, int y2, int z2, float c): 
     x1{x1}, y1{y1}, z1{z1}, x2{x2}, y2{y2}, z2{z2}, c{c} {}
 };
