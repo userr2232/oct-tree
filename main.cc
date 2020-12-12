408d08d61daed7e99300016680ba6e9c135a6677
userr2232 <reynaldo.rz.26@gmail.com>
2020-12-12 16:18:41 -0500

    before compilation

--- main.cc
+++ main.cc
@@ -3,14 +3,16 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <utility>
 
 using namespace std;
-#include <fstream>
+
 class Octree {
     private:
         fstream save_file;
         long EPS = 2;
-        Node* root{0};
+        Node root;
 
         vector<const char*> get_file_names(const char* fnames) {
             string fname;
@@ -25,13 +27,17 @@ class Octree {
             vector<CImg<unsigned char>> images;
             for(auto x : v)
                 images.push_back(CImg<unsigned char>(x));
-            root = new Node(0, 0, 0, images[0].width(), images[0].height(), images.size(), images[0](0,0));
+            allocate_node(0, 0, 0, images[0].width(), images[0].height(), images.size(), images);
+            read_root();
             insert(root, images);
         }
 
         Node read(long dir)
         {
-            //todo
+            save_file.seekg(dir);
+            Node node{};
+            save_file.read((char*)(&node),sizeof(Node));
+            return node;
         }
 
         void insert(Node cur, vector<CImg<unsigned char>>& cube) {
@@ -70,37 +76,19 @@ class Octree {
             int z1=cur.z1;
             int z2=cur.z2;
 
-
-
             long xm=(cur.x1+cur->x2)/2;
             long ym=(cur.y1+cur->y2)/2;
             long zm=(cur->z1+cur->z2)/2;
 
-            allocate_node(x1,y1,z1,xm,ym,zm);
-            allocate_node(x1,ym,z1,xm,y2,zm);
-            allocate_node(xm,ym,z1,x2,y2,zm);
-            allocate_node(xm,y1,z1,x2,ym,zm);
+            cur->children[0] = allocate_node(x1,y1,z1,xm,ym,zm);// AR1(AMARILLO)=X1,Y1,Z1 -> XM,YN,ZM
+            cur->children[1] = allocate_node(x1,ym,z1,xm,y2,zm);//AR2(MORADO)=X1,YM,Z1 -> XM,Y2,ZM
+            cur->children[2] = allocate_node(xm,ym,z1,x2,y2,zm);//AR3(VERDE)=XM,YM,Z1 -> X2,Y2,ZM
+            cur->children[3] = allocate_node(xm,y1,z1,x2,ym,zm);//ARR4(CELESTE)=XM,Y1,Z1 -> X2,YM,ZM
 
-            allocate_node(x1,y1,zm,xm,ym,z2);
-            allocate_node(x1,ym,zm,xm),y2,z2);
-            allocate_node();
-            
-
-            /*
-                AR1(AMARILLO)=X1,Y1,Z1 -> XM,YN,ZM
-                AR2(MORADO)=X1,YM,Z1 -> XM,Y2,ZM
-                AR3(VERDE)=XM,YM,Z1 -> X2,Y2,ZM
-                ARR4(CELESTE)=XM,Y1,Z1 -> X2,YM,ZM
-                
-                AB1(AMARILLO)= X1, Y1,ZM -> XM,YM,Z2
-                AB2(BLANCO)= X1,YM,ZM -> XM,Y2,Z2
-                AB3(CELESTE)=XM,Y1,ZM -> X2,YM,Z2
-                AB4(VERDE)=XM,YM,ZM -> X2,Y2,Z2
-
-
-            */
-
-        
+            cur->children[4] = allocate_node(x1,y1,zm,xm,ym,z2);//AB1(AMARILLO)= X1, Y1,ZM -> XM,YM,Z2
+            cur->children[5] = allocate_node(x1,ym,zm,xm,y2,z2);//AB2(BLANCO)= X1,YM,ZM -> XM,Y2,Z2
+            cur->children[6] = allocate_node(xm,y1,zm,x2,ym,z2);//AB3(CELESTE)=XM,Y1,ZM -> X2,YM,Z2
+            cur->children[7] = allocate_node(xm,ym,zm,x2,y2,z2);//AB4(VERDE)=XM,YM,ZM -> X2,Y2,Z2
         }
 
         pair<bool,long> verificar(long x1, long y1, long z1, long x2, long y2, long z2, vector<CImg<unsigned char>>& cube) {
@@ -118,23 +106,20 @@ class Octree {
             }
             return {true,color/((x2-x1)*(y2-y1)*(z2-z1));
         }
-        void save(Node* cur)
-        {
-            if(!cur) return;
-            cur->save();
-            for(long i=0;i<8;i++)
-                save(cur->children[i]);
-        };
 
+        void read_root() {
+            root = read(0);
+        }
 
     public:
         Octree(const char* nodesfile) {
-            load(nodesfile);
+            save_file.open(nodesfile, fstream::in | fstream::out);
+            read_root();
+            save_file.close();
         }
         Octree(const char* nodesfile, const char* fnames) {
-            save_file.open(nodesfile);
+            save_file.open(nodesfile, fstream::in | fstream::out);
             build(fnames);
-            save(root);
+            save_file.close();
         }
-
 };
--- node.h
+++ node.h
@@ -4,6 +4,7 @@ struct Node {
 
   long children[8];
 
+  Node() = default;
   Node(int x1, int y1, int z1, int x2, int y2, int z2, int c): 
     x1{x1}, y1{y1}, z1{z1}, x2{x2}, y2{y2}, z2{z2}, c{c} {}
 };
